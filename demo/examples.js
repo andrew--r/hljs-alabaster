a0c85d9fd5d53e74589c89bec85f7603577368c8
Andrey Romanov <me@andreyromanov.com>
2020-01-04 13:29:23 +0300

    Fix Python highlight example

--- demo/examples.js
+++ demo/examples.js
@@ -61,7 +61,7 @@ window.hljsExamples.python = `@requires_authorization
 def somefunc(param1='', param2=0):
     r'''A docstring'''
     if param1 > param2: # interesting
-        print 'Gre\'ater'
+        print 'Gre\\'ater'
     return (param2 - param1 + 1 + 0b10l) or None
 
 class SomeClass:
